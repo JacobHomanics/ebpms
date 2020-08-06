871c21edfece81be1f7e5a785d9f3f071bb20bb7
Jacob Homanics <homanicsjake@gmail.com>
2020-08-06 10:45:40 -0400

    hold onto this

---
+++ ebpms-u/Assets/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Movement : MonoBehaviour
+{
+	public UnityEvent OnThresholdReached;
+
+    void Update()
+    {
+		transform.Translate(Vector3.forward * Time.deltaTime);
+		if (transform.position.z > 3f)
+			OnThresholdReached?.Invoke();
+	}
+}
---
+++ ebpms-u/Assets/TransformSpawner.cs
@@ -0,0 +1,10 @@
+﻿using JacobHomanics.Core.PoolManagement;
+using UnityEngine;
+
+public class TransformSpawner : MonoBehaviour
+{
+    public void OnSpawn(PoolEntityManager manager, PoolEntity entity)
+	{
+		entity.transform.position = transform.position;
+	}
+}
