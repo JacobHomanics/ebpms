c01e5c89c9a8d216b3feda2222a5e580e4e4e299
LAPTOP-MFIQ4P2Q\Jacob <homanicsjake@gmail.com>
2021-06-10 09:55:09 -0400

    update

--- ebpms-u/Assets/JacobHomanics/Core/Pool Management/Scripts/Entity/PoolEntity.cs
+++ ebpms-u/Assets/JacobHomanics/Core/Pool Management/Scripts/Entity/PoolEntity.cs
@@ -5,10 +5,12 @@ namespace JacobHomanics.Core.PoolManagement
 {
 	public class PoolEntity : MonoBehaviour
 	{
-		public Spawned Spawned;
-		public Despawned Despawned;
-		public Initialized Initialized;
-		public Terminated Terminated;
+		public Events Events = new Events();
+
+		public Initialized Initialized { get { return Events.Initialized; } set { Events.Initialized = value; } }
+		public Spawned Spawned { get { return Events.Spawned; } set { Events.Spawned = value; } }
+		public Despawned Despawned { get { return Events.Despawned; } set { Events.Despawned = value; } }
+		public Terminated Terminated { get { return Events.Terminated; } set { Events.Terminated = value; } }
 
 		public PoolEntityManager PoolEntityManager { get; private set; }
 
